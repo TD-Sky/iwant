dd5c7aefa53e69bea77c4556703d50d9deee1386
TD-Sky <three-dim-sky@foxmail.com>
2023-04-03 00:14:31 +0800

    feat(completions): ignore completions

--- build.rs
+++ build.rs
@@ -4,8 +4,9 @@ mod cli;
 use self::cli::Cli;
 use clap::CommandFactory;
 use clap_complete::generate_to;
-use clap_complete::Shell::{Bash, Zsh};
+use clap_complete::Shell::{Bash, Fish, Zsh};
 use clap_complete_nushell::Nushell;
+use std::fs;
 use std::io;
 
 fn main() -> io::Result<()> {
@@ -13,7 +14,10 @@ fn main() -> io::Result<()> {
     let name = "iwant";
     let dir = "completions";
 
+    fs::create_dir_all(dir)?;
+
     generate_to(Bash, cmd, name, dir)?;
+    generate_to(Fish, cmd, name, dir)?;
     generate_to(Zsh, cmd, name, dir)?;
     generate_to(Nushell, cmd, name, dir)?;
 
