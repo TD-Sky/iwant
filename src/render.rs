75110f506029b11cc5d666d684429765bbe1d3c5
TD-Sky <three-dim-sky@foxmail.com>
2023-06-11 23:44:48 +0800

    feat(wrap line): wrap the table line in case it's too long

--- src/render.rs
+++ src/render.rs
@@ -1,15 +1,23 @@
 use crate::item::Item;
-use tabled::object::Rows;
-use tabled::Alignment;
-use tabled::Modify;
-use tabled::Style;
+
+use tabled::settings::Settings;
+use tabled::settings::Style;
+use tabled::settings::{object::Rows, Alignment, Modify};
+use tabled::settings::{peaker::PriorityMax, Width};
 use tabled::Table;
-use tabled::TableIteratorExt;
+use terminal_size::terminal_size;
+
+#[inline]
+fn get_terminal_width() -> usize {
+    terminal_size().expect("STDOUT is not a tty").0 .0 as usize
+}
 
 pub fn to_table(items: &[Item<'_>]) -> Table {
-    let mut table = items.table();
-    table
+    let mut table = Table::new(items);
+    let settings = Settings::default()
         .with(Style::rounded())
-        .with(Modify::new(Rows::first()).with(Alignment::center()));
+        .with(Modify::new(Rows::first()).with(Alignment::center()))
+        .with(Width::wrap(get_terminal_width()).priority::<PriorityMax>());
+    table.with(settings);
     table
 }
